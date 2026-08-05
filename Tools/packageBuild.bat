94ec1bc09dd7ddcdacfd3e83d85c61024087ef10
Ricchio <ricchio@gmail.com>
2026-08-04 20:22:16 -0700

    Updated UAT command to use variables for different dirs
    Added prompts and defaults for:
    - ProjectAtlantis.uproject
    - UAT.bat
    - UnrealEditor-Cmd.exe

--- Tools/packageBuild.bat
+++ Tools/packageBuild.bat
@@ -1,13 +1,35 @@
 @echo off
-REM Defaults (for Ricchio)
+echo.
+
+REM Defaults
+set UPROJECT=../ProjectAtlantis.uproject
+for %%I in ("%UPROJECT%") do set "UPROJECT=%%~fI"
+set UAT=C:\Program Files\Epic Games\UE_5.7\Engine\Build\BatchFiles\RunUAT.bat
+set EDITOR=C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor-Cmd.exe
 set BASEDIR=C:\ASGC\GameJam2026\PackagedBuilds
+set VER=
 
 REM Get directory and version info
 echo *** GET DIRECTORY AND VERSION INFO ***
+echo Where does your ProjectAtlantis.uproject live?
+echo [default is %UPROJECT%]
+set /p UPROJECT="Enter the full path to your ProjectAtlantis.uproject: "
+echo UPROJECT IS (%UPROJECT%)
+echo.
+echo Where does your UAT.bat live?
+echo [default is C:\Program Files\Epic Games\UE_5.7\Engine\Build\BatchFiles\RunUAT.bat]
+set /p UAT="Enter the full path to your RunUAT.bat: "
+echo UAT IS (%UAT%)
+echo.
+echo Where does your UnrealEditor-Cmd.exe live?
+echo [default is C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor-Cmd.exe]
+set /p EDITOR="Enter the full path to your UnrealEditor-Cmd.exe: "
+echo EDITOR IS (%EDITOR%)
+echo.
 echo Where do you want to create the package? (must match default below)
 echo [default is C:\ASGC\GameJam2026\PackagedBuilds]
 set /p BASEDIR="Enter the full path to your package builds directory: "
-echo PACKAGEBUILDS DIR IS (%BASEDIR%)
+echo PACKAGE BUILDS DIR IS (%BASEDIR%)
 echo.
 echo What is the GitHub tag to create/use for this package? (e.g. 0.0.1)
 set /p VER="Enter the GitHub tag (i.e. version): "
@@ -24,8 +46,9 @@ if not exist "%FULLDIR%\" mkdir "%FULLDIR%"
 echo.
 
 REM Create the Package using UAT
+REM This is just a straight copy of the output log when building from the Editor.  No doubt it can be refined.
 echo *** CREATE PACKAGE WITH UAT ***
-set CMD=cmd.exe /c ""C:/Program Files/Epic Games/UE_5.7/Engine/Build/BatchFiles/RunUAT.bat"  -ScriptsForProject="C:/GitHub/ASGC-Game-Jam/asgc-gamejam-2026/ProjectAtlantis.uproject" Turnkey -command=VerifySdk -platform=Win64 -UpdateIfNeeded -EditorIO -EditorIOPort=54650  -project="C:/GitHub/ASGC-Game-Jam/asgc-gamejam-2026/ProjectAtlantis.uproject" BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook  -project="C:/GitHub/ASGC-Game-Jam/asgc-gamejam-2026/ProjectAtlantis.uproject"  -unrealexe="C:\Program Files\Epic Games\UE_5.7\Engine\Binaries\Win64\UnrealEditor-Cmd.exe" -platform=Win64 -installed -SkipCookingErrorSummary -JsonStdOut -stage -archive -package -build -pak -iostore -compressed -prereqs -archivedirectory="C:\ASGC\GameJam2026\PackagedBuilds\ProjectAtlantis-0.0.1-Windows\Windows" -clientconfig=Shipping -nodebuginfo" -nocompile -nocompileuat
+set CMD=cmd.exe /c ""%UAT%"  -ScriptsForProject="%UPROJECT%" Turnkey -command=VerifySdk -platform=Win64 -UpdateIfNeeded -EditorIO -EditorIOPort=54650  -project="%UPROJECT%" BuildCookRun -nop4 -utf8output -nocompileeditor -skipbuildeditor -cook  -project="%UPROJECT%"  -unrealexe="%EDITOR%" -platform=Win64 -installed -SkipCookingErrorSummary -JsonStdOut -stage -archive -package -build -pak -iostore -compressed -prereqs -archivedirectory="%FULLDIR%\Windows" -clientconfig=Shipping -nodebuginfo" -nocompile -nocompileuat
 echo UAT CMD IS: (%CMD%)
 %CMD%
 echo.
