d360812729fc51ae5a00b1d8b0e4c3a5675af041
clarasmith-sound <clarasmith.sound@proton.me>
2026-08-14 10:43:58 -0700

    Audio Init (#56)
    Contains basic audio setup including:
    - Adding in epic games plugins (Soundscape, AudioModulation,
    AudioGameplay, AudioGameplayVolume, Harmonix, Soundscape,
    WaveformEditor) [all used in Fortnite fyi]
    - set up basic submix structure
    - set up basic sound class structure
    - added in user settings sliders using control bus modulation
    - added in a level for audio testing
    - added in stub for custom audio subsystem in C++
    - added in defaults for attenuation and concurrency
    - set up folder hierarchy and used some temp assets to hold folders for
    git
    - updated engine audio settings to be more reflective of the project
    
    # Summary
    Tech Audio init commit, to help give us a framework for tasking out sub
    tasks in tech audio.
    
    
    # Related Issue
    Related to #55 
    Closes #55 
    
    # Testing
    
    - [x] Built successfully
    - [x] Tested locally
    - [x] No known regressions
    
    # Checklist
    
    - [x] PR title accurately summarizes the final change
    - [x] Summary reflects what was actually implemented
    - [x] Follows the project style guide
    - [x] Documentation updated if needed
    - [x] No unrelated changes included
    - [x] Ready for review

---
+++ Source/ProjectAtlantis/Public/Audio/AtlantisAudioSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "AtlantisAudioSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTATLANTIS_API UAtlantisAudioSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
