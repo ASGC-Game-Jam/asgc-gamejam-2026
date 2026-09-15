bcb9075c7cbd576de4dcd35476f90ac25cfd44b1
Kevin <lekev001@gmail.com>
2026-09-15 12:55:34 -0600

    feat: Update last commit to now-current standards.

---
+++ Source/ProjectAtlantis/Private/Core/AtlantisAIController.cpp
@@ -0,0 +1,10 @@
+﻿#include "Core/AtlantisAIController.h"
+
+#include "Components/StateTreeAIComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+
+AAtlantisAIController::AAtlantisAIController()
+{
+	AIPerception = CreateDefaultSubobject<UAIPerceptionComponent>("AI Perception");
+	StateTreeComponent = CreateDefaultSubobject<UStateTreeAIComponent>("State Tree");
+}
---
+++ Source/ProjectAtlantis/Public/Core/AtlantisAIController.h
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2026 ASGC
+
+#pragma once
+#include "AIController.h"
+
+#include "AtlantisAIController.generated.h"
+
+class UAIPerceptionComponent;
+class UStateTreeAIComponent;
+
+UCLASS()
+class PROJECTATLANTIS_API AAtlantisAIController : public AAIController
+{
+	GENERATED_BODY()
+public:
+	AAtlantisAIController();
+
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Atlantis|AI")
+	TObjectPtr<UAIPerceptionComponent> AIPerception;
+	
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Atlantis|AI")
+	TObjectPtr<UStateTreeAIComponent> StateTreeComponent;
+};
